b7f18e6547ab6f34b1915c35b11103361abcf4dc
Dimitris Gkoulis <gkould@gmail.com>
2026-01-25 17:16:24 +0200

    v0.1.37

--- src/modules/food-quiz-story-mode/views/FoodQuizStoryModeSeriousGameGameplayView.vue
+++ src/modules/food-quiz-story-mode/views/FoodQuizStoryModeSeriousGameGameplayView.vue
@@ -8,11 +8,13 @@ const router = useRouter();
 
 const THEME_ID = route.params.themeId;
 const THEME_REF = ref(null);
+const userIdRef = ref(null);
 
 onBeforeMount(() => {
   THEME_REF.value = null;
 
   const languageCode = localStorage.getItem("languageCode");
+  userIdRef.value = localStorage.getItem("userId") || null;
 
   let key = `food-quiz-story-mode-data-THEME_BY_ID_REF-${languageCode}`;
   const jsonString = localStorage.getItem(key);
@@ -53,7 +55,7 @@ onBeforeMount(() => {
 
 <template>
   <template v-if="THEME_REF">
-    <FoodQuizStoryModeSeriousGameTheme :theme="THEME_REF" />
+    <FoodQuizStoryModeSeriousGameTheme :theme="THEME_REF" :user-id="userIdRef" />
   </template>
 </template>
 
